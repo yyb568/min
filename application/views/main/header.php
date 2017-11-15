e3d62132c50834a0f9560d632f2a02b179ee2628
yyb5683 <846373066@qq.com>
2017-11-15 22:03:54 +0800

    d

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -10,8 +10,8 @@
     <link href="<?=static_url("css")?>font-awesome.min.css"  rel="stylesheet">
     <link href="<?=static_url("css")?>animate.min.css"  rel="stylesheet">
     <link href="<?=static_url("css")?>style.min.css"  rel="stylesheet">
-    <link href="<?=static_url("css") ?>/plugins/toastr/toastr.min.css" rel="stylesheet">
-    <link href="<?=static_url("js") ?>/plugins//layer/skin/layer.css" rel="stylesheet">
+    <link href="<?=static_url("css") ?>plugins/toastr/toastr.min.css" rel="stylesheet">
+    <link href="<?=static_url("js") ?>plugins//layer/skin/layer.css" rel="stylesheet">
     <style>
     .hong{
     padding:10px 20px 10px 20px;
